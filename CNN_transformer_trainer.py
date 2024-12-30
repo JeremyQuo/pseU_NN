9ae2f5b70bfcdc63469300333fb8aacc7ee69c91
GUO ZHIHAO <qhuozhihao@icloud.com>
2024-12-30 17:48:08 +0800

    add k-folder

--- CNN_transformer_trainer.py
+++ CNN_transformer_trainer.py
@@ -2,25 +2,27 @@ import torch
 import pandas as pd
 import numpy as np
 import os
-import networkx as nx
-from torch_geometric.data import Data, DataLoader
+
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
 from torch.nn import functional as F
 import random
+
 # 设置随机种子
-seed = 24
+seed = 42
 torch.manual_seed(seed)          # 设置 PyTorch 随机种子
 np.random.seed(seed)             # 设置 NumPy 随机种子
 random.seed(seed)
+DEVICE_NUM = 'cuda:0'
 
 BASES = ['A', 'U', 'C', 'G']
 BASE_TO_INDEX = {base: idx for idx, base in enumerate(BASES)}
 SEQUENCE_LENGTH = 41
 NUM_CLASSES = 2
 from sklearn.metrics import roc_auc_score
-def read_file(directory_path, label, device=None):
-    if device is None:
-        device = torch.device('cuda:0')
-    
+
+
+def read_file(directory_path, label):
     file_names = os.listdir(directory_path)
     data_list = []
     num_file = 0
@@ -28,8 +30,6 @@ def read_file(directory_path, label, device=None):
     for file_name in file_names:
         if num_file >= 1000:
             break
-        num_file += 1
-        
         # Read file
         file_path = os.path.join(directory_path, file_name)
         df = pd.read_csv(file_path, skiprows=1, header=None, sep='\t')
@@ -62,13 +62,13 @@ def read_file(directory_path, label, device=None):
         adj_matrix = np.hstack([adj_matrix, one_hot.numpy()])
         
         # Convert to tensors and move to device
-        x = torch.tensor(adj_matrix, dtype=torch.float32).unsqueeze(0).to(device)
-        y = torch.tensor([[label]], dtype=torch.float).to(device)
+        x = torch.tensor(adj_matrix, dtype=torch.float32).unsqueeze(0)
+        y = torch.tensor([[label]], dtype=torch.float)
         
         # Create Data object
         data = Data(x=x, y=y)
         data_list.append(data)
-    
+        num_file += 1
     return data_list
 
 
@@ -98,40 +98,22 @@ class GraphCNN(nn.Module):
         adj_x = self.pool(F.relu(self.conv2(adj_x)))
         adj_x = adj_x.view(-1, 8 * (num_nodes // 2) * (num_nodes // 2))  # 展平
         adj_x = F.relu(self.fc1(adj_x))
-        adj_x = F.relu(self.fc2(adj_x))
         adj_x = self.dropout(adj_x)
+        adj_x = F.relu(self.fc2(adj_x))
         seq_x = self.transformer(seq_x)  # [batch_size, seq_len, hidden_dim]
         seq_x = seq_x.mean(dim=1)  # Pool over the sequence dimension
-        seq_x = self.dropout(seq_x)
+        # seq_x = self.dropout(seq_x)
         x = torch.cat((adj_x, seq_x), dim=1)
         return self.Sigmoid(self.fc3(x))
 
-
-
-# Step 4: Train the model
-def train_model(model, data_loader, optimizer, criterion):
-    model.train()
-    total_loss = 0
-
-    for batch in data_loader:
-        optimizer.zero_grad()
-        out = model(batch.x,batch.batch_size)
-        loss = criterion(out, batch.y)
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item()
-        # print(total_loss)
-
-    return total_loss / len(data_loader)
-
-# 验证函数
  
 # Step 4: Train the model
-def train_model(model, data_loader, optimizer, criterion):
+def train_model(model, data_loader, optimizer, criterion,device):
     model.train()
     total_loss = 0
 
     for batch in data_loader:
+        batch = batch.to(device)
         optimizer.zero_grad()
         out = model(batch.x,batch.batch_size)
         loss = criterion(out, batch.y)
@@ -143,45 +125,44 @@ def train_model(model, data_loader, optimizer, criterion):
     return total_loss / len(data_loader)
 
 # 验证函数
-def evaluate_model(model, data_loader):
+def evaluate_model(model, data_loader,device):
     model.eval()
-    correct = 0
-    total = 0
-    # 只取正类的概率
-    #
-    # # 计算 AUC-ROC
-    # auc_roc = roc_auc_score(y_test, y_pred_proba)
-
-    # return auc_roc
     with torch.no_grad():
         for batch in data_loader:
+            batch = batch.to(device)
             out = model(batch.x, batch.batch_size)
             auc_roc = roc_auc_score(batch.y.cpu(), out.cpu())
-
-
     return auc_roc
     
-positive_file = read_file('data/mlf/bpseq',1)
-negative_file = read_file('data/neg/bpseq',0)
+positive_file = read_file('data/mlf/bpseq',1,)
+negative_file = read_file('data/neg/bpseq',0,)
 
-device=torch.device('cuda:0')
+device=torch.device(DEVICE_NUM)
 positive_file.extend(negative_file)
-
 random.shuffle(positive_file)
-split_index = int(0.8 * len(positive_file))
-train_list = positive_file[:split_index]
-test_list = positive_file[split_index:]
-train_dataset = DataLoader(train_list, batch_size=800, shuffle=True)
-test_dataset = DataLoader(test_list, batch_size=len(test_list), shuffle=True)
-# 实例化模型
-model = GraphCNN().to(device)
 
-# 定义损失函数和优化器
-criterion = nn.BCELoss().to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+feature_list = positive_file
+k = 5  # K 的值
+roc_auc_list=[]
+for i in range(k):
+    unit_number = len(feature_list)//k
+    test_list = feature_list[i*unit_number:(i+1)*unit_number]
+    train_list = feature_list[0:i*unit_number]
+    train_list.extend(feature_list[(i+1)*unit_number:])
 
-# 训练流程
-for epoch in range(30):  # 20个训练轮次
-    train_loss = train_model(model, train_dataset, optimizer, criterion)
-    auc_roc = evaluate_model(model, test_dataset)
-    print(f"Epoch {epoch+1}, Loss: {train_loss:.4f},AUC_ROC: {auc_roc:.4f}")
+    model = GraphCNN().to(device)
+    criterion = nn.BCELoss().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=0.0001)
+    train_dataset = DataLoader(train_list, batch_size=50, shuffle=True)
+    test_dataset = DataLoader(test_list, batch_size=len(test_list), shuffle=True)
+    best_roc_auc=0
+    for epoch in range(50):  # 20个训练轮次
+        train_loss = train_model(model, train_dataset, optimizer, criterion,DEVICE_NUM)
+        auc_roc = evaluate_model(model, test_dataset,DEVICE_NUM)
+        if auc_roc > best_roc_auc:
+            best_roc_auc = auc_roc
+        print(f"Epoch {epoch + 1}, Loss: {train_loss:.4f},AUC_ROC: {auc_roc:.4f}")
+    roc_auc_list.append(best_roc_auc)
+
+print(np.mean(roc_auc_list))
+
