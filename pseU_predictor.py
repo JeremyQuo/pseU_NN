e3d1e985a1d928ff21c664e018c322d308eb2140
GUOZHIHAO <qhuozhihao@iclou.com>
2025-01-09 14:58:44 +0800

    add tqdm function

--- pseU_predictor.py
+++ pseU_predictor.py
@@ -5,6 +5,7 @@ import os
 from torch_geometric.data import Data
 from torch_geometric.loader import DataLoader
 from model import GraphCNN
+from tqdm import tqdm
 import torch
 
 BASES = ['A', 'U', 'C', 'G']
@@ -64,26 +65,31 @@ def main(input_file, model_file, output_file, batch_size, device,file_number):
     input_file,file_names = read_file(input_file,file_number)
     model = torch.load(model_file, map_location=torch.device(device))
     test_dataset = DataLoader(input_file, batch_size=batch_size, shuffle=True)
+    batch_number = len(input_file)//batch_size + 1
+    print('Start to predict ...')
+    pbar = tqdm(range(batch_number),unit='batch')
     with torch.no_grad():
         result = torch.tensor([])
         for batch in test_dataset:
             batch = batch.to(device)
             out = model(batch.x, batch.batch_size)
             result = torch.cat((result, out), dim=0)
+            pbar.update(1)
+    pbar.close()
     df = pd.DataFrame(result.numpy(),columns=['predictions'])
     df['seq_name'] = file_names
     df = df[['seq_name','predictions']]
     if output_file.split('.')[-1]!='csv':
         output_file = output_file+'.csv'
     df.to_csv(output_file,index=False)
-    print(1)
+    print('Saved as '+ output_file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i','--input_folder', type=str, help='Directory path to input files')
     parser.add_argument('--model', type=str, help='Path to the model file',default='model.pth')
     parser.add_argument('-o','--output', type=str, help='Output csv path',default='output.csv')
-    parser.add_argument('--batch_size', type=int, help='batch_size of input feature', default=200)
+    parser.add_argument('--batch_size', type=int, help='batch_size of input feature', default=500)
     parser.add_argument('--subsample_number', type=int, help='Subsample number if required', default=None)
     parser.add_argument('--device', type=str, help='Device name', default='cpu')
     args = parser.parse_args()
